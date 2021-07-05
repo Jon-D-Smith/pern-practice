160d425998b0b7d1d843c18984fe232368024067
Jon Smith <aardvarkjon94@yahoo.com>
2021-07-05 16:32:25 -0500

    Setup nodemon and dotenv

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const PORT = 3000
 
+app.get('/', (req, res) => {
+
+})
+
+const PORT = process.env.PORT || 3000
 app.listen(PORT,() => {
     console.log(`Connected on port ${PORT}`)
 })
