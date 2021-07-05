a7069e07dd70624ba812d00d9cc52976d3281cb8
Jon Smith <aardvarkjon94@yahoo.com>
2021-07-05 17:45:16 -0500

    Setup skeleton routes

--- server/server.js
+++ server/server.js
@@ -2,8 +2,47 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
+app.use(express.json())
 
+//Get All Restaurants
+app.get('/api/v1/restaurants', (req, res) => {
+    res.status(200)
+       .json({
+           status: "Success",
+           data: {
+               restaurant: ['McDonalds', 'Wendys']
+           }
+       }
+
+    )
+})
+
+//Getting an individual restaurant
+app.get('/api/v1/restaurants/:id', (req, res) => {
+    res.status(200).json({
+        status: "Success",
+        data: {
+            restaurant: {name: 'McDonalds', city: 'Denver'}
+        }
+    })
+})
+
+//Create a restaurant
+app.post('/api/v1/restaurants', (req, res) => {
+    console.log(req.body)
+    res.json(req.body)
+})
+
+// Update a restaurant
+
+app.put('/api/v1/restaurants/:id', (req, res) => {
+    console.log(req.params.id)
+    console.log(req.body)
+})
+
+//Delete a restaurant
+app.delete('/api/v1/restaurants/:id', (req, res) => {
+    console.log(req.params.id)
 })
 
 const PORT = process.env.PORT || 3000
