7d940020d8b450bbac7127e80830790b4ed4be2d
Jon Smith <aardvarkjon94@yahoo.com>
2021-07-05 15:56:01 -0500

    wrote out basic express server

---
+++ db.sql
@@ -0,0 +1,45 @@
+                                                    -- Notes Section -- 
+-- List dbs 
+\l
+-- Create a db 
+CREATE DATABASE dbname
+-- List all tables 
+\d 
+-- List db columns
+\d [table_name]
+
+
+-- Create table in database
+CREATE TABLE products(
+    id INT,
+    name VARCHAR(50),
+    price INT,
+    on_sale BOOLEAN  
+);
+
+-- Connect to a database
+\c [db name]
+
+-- Add COLUMNs
+
+ALTER TABLE products ADD COLUMN on_sale BOOLEAN;
+ALTER TABLE products ADD COLUMN featured BOOLEAN;
+
+--Drop columns
+
+ALTER TABLE products DROP COLUMN featured;
+
+-- Delete Database
+DROP DATABASE [db name]
+
+
+ -- CREATING THE YELP DB
+
+ CREATE TABLE restaurants (
+     id BIGSERIAL NOT NULL PRIMARY KEY,
+     name VARCHAR(50) NOT NULL,
+     location VARCHAR(50) NOT NULL,
+     price_range INT NOT NULL check(price_range >= 1 and price_range <= 5)
+ );
+
+ INSERT INTO restaurants(id, name, location, price_range) VALUES (123, 'McDonalds', 'New York', 3);
