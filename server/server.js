a264933a5a10086a43245ec38fedc0eda7883b57
Jon Smith <aardvarkjon94@yahoo.com>
2021-07-06 19:16:31 -0500

    Finished restaurant post route

--- server/server.js
+++ server/server.js
@@ -25,22 +25,24 @@ app.get('/api/v1/restaurants', async (req, res) => {
 //Getting an individual restaurant
 app.get('/api/v1/restaurants/:id', async (req, res) => {
     const { id } = req.params
-    const result = await db.query('SELECT * FROM restaurants WHERE id = $1', [id])
+    const results = await db.query('SELECT * FROM restaurants WHERE id = $1', [id])
 
     res.status(200).json({
         status: "Success",
         data: {
-            restaurant: result.rows
+            restaurant: results.rows[0]
         }
     })
 })
 
 //Create a restaurant
-app.post('/api/v1/restaurants', (req, res) => {
+app.post('/api/v1/restaurants', async (req, res) => {
+    const {name, location, price_range} = req.body
+    const results = await db.query('INSERT INTO restaurants(name, location, price_range) VALUES($1, $2, $3) returning *', [name, location, price_range])
     res.status(201).json({
         status: "Success",
         data: {
-            restaurant: {name: 'McDonalds', city: 'Denver'}
+            restaurant: results.rows[0]
         }
     })
 })
