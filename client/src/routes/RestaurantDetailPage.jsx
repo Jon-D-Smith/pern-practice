6eeec1871598258c07271e2c8bc673bf2203fae4
Jon Smith <aardvarkjon94@yahoo.com>
2021-07-13 18:34:24 -0500

    Setup detail page useEffect

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -1,7 +1,26 @@
+import { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import RestaurantFinder from "../apis/RestaurantFinder";
+import { RestaurantsContext } from "../context/RestaurantsContext";
+
 const RestaurantDetailPage = () => {
+    const {id} = useParams()
+    const {selectedRestaurant, setSelectedRestaurant} = useContext(RestaurantsContext)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const response = await RestaurantFinder.get(`/${id}`)
+                setSelectedRestaurant(response.data.data.restaurant)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        fetchData()
+    },[])
     return ( 
         <div>
-            Detail Page
+            {selectedRestaurant && selectedRestaurant.name}
         </div>
      );
 }
