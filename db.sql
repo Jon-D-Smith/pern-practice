429f469dbddeca5cced3d362bd2d8f536a3f65a4
Jon Smith <aardvarkjon94@yahoo.com>
2021-07-14 12:54:25 -0500

    added postgres notes for DB

--- db.sql
+++ db.sql
@@ -43,3 +43,13 @@ DROP DATABASE [db name]
  );
 
  INSERT INTO restaurants(id, name, location, price_range) VALUES (123, 'McDonalds', 'New York', 3);
+
+
+-- Creating reviews table with a foreign key to the restaurants
+ CREATE TABLE reviews (
+     id BIGSERIAL NOT NULL PRIMARY KEY,
+     restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
+     name VARCHAR(50) NOT NULL,
+     review TEXT NOT NULL,
+     rating INT NOT NULL check(rating >= 1 AND rating <= 5)
+ );
